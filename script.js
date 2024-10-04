80907eb0def80734ab554dedfd93fae4a05d1fa4
Manish Pandey <163403001+manishpandey68@users.noreply.github.com>
2024-10-04 23:15:17 +0545

    changged

---
+++ script.js
@@ -0,0 +1,6 @@
+const cont = document.getElementById('kontact');
+
+cont.addEventListener('click',()=>{
+    // alert("Send email at pandeymanish2062@gmail.com");
+        window.location.href = "contact.html";
+})
