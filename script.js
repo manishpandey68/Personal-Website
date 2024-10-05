b617daaf6d9407478fc404317e10da817ab4bcb3
Manish Pandey <163403001+manishpandey68@users.noreply.github.com>
2024-10-05 16:00:13 +0545

    again

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const cont = document.getElementById('kontact');
+const cont = document.getElementById('contact');
 
 cont.addEventListener('click',()=>{
     // alert("Send email at pandeymanish2062@gmail.com");
         window.location.href = "contact.html";
+        cont.style.cursor= "pointer";
 })  
